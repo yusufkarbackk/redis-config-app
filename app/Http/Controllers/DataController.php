d3e35c9847427ed789d9db904b70c67b77bf006e
yusufkarbackk <yusufkarback64@gmail.com>
2025-01-15 17:05:06 +0700

    can send stream data to redis

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -6,9 +6,12 @@ use App\Models\Application;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 
+use function PHPSTORM_META\type;
+
 class DataController extends Controller
 {
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         //validate API key
 
         $apiKey = $request->header('X-API-Key');
@@ -19,16 +22,18 @@ class DataController extends Controller
         $incomingData = $request->all();
         //dd($validFields);
         $filteredData = array_intersect_key($incomingData, array_flip($validFields));
-        dd($filteredData);
-        //add to redis stream
-        $redis = Redis::connection();
+        //dd(print_r($filteredData));
+        
         //dd($application);
         $id = $application->getAttributes()['id'];
         $streamKey = "app:{$id}:stream";
-        $MessageId = $redis->xAdd(
-            $streamKey,
-            "*",
-            $filteredData
+        //dd($streamKey);
+        $MessageId = Redis::command(
+            'xadd',
+            [
+                $streamKey,
+                $filteredData,
+            ]
         );
 
         return response()->json([
