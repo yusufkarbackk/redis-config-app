b9eebf0b9d54d2116e2a881530e7a01d4df86051
yusufkarbackk <yusufkarback64@gmail.com>
2025-02-07 17:17:58 +0700

    membuat log untuk mencatat aksi dan membuat index api_key

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Application;
+use App\Models\Log;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 
@@ -12,34 +13,45 @@ class DataController extends Controller
 {
     public function store(Request $request)
     {
+        $log = new Log();
+
         //validate API key
 
         $apiKey = $request->header('X-API-Key');
-        $application = Application::where('api_key', $apiKey)->firstOrFail();
+        $application = Application::where('api_key', $apiKey)->select(['api_key', 'id'])->firstOrFail();
         //validate incoming fields against configured fields
         $validFields = $application->applicationFields()->pluck('name')->toArray();
-        $incomingData = $request->all();
 
-        $filteredData = array_intersect_key($incomingData, array_flip($validFields));
+        //$filteredData = array_intersect_key($incomingData, array_flip($validFields));
 
         $id = $application->getAttributes()['api_key'];
         $streamKey = "app:{$id}:stream";
 
-        $MessageId = Redis::command(
-            'xadd',
-            [   
-                $streamKey,
-                '*',
-                $filteredData,
-            ]
-        );
+        $filteredData = $request->only($validFields);
 
-        //dd($MessageId);
-        return response()->json([
-            'message' => 'Data received and queued',
-            'message_id' => $MessageId,
-            "data" => $filteredData
-            
-        ]);
+        try {
+            $MessageId = Redis::command(
+                'xadd',
+                [
+                    $streamKey,
+                    '*',
+                    $filteredData,
+                ]
+            );
+
+            $log->log = "send data to Redis Server";
+            $log->save();
+
+            //dd($MessageId);
+            return response()->json([
+                'message' => 'Data received and queued',
+                'message_id' => $MessageId,
+                "data" => $filteredData
+
+            ]);
+        } catch (\Throwable $th) {
+            $log->log = $th->getMessage() ;
+            $log->save();
+        }
     }
 }
