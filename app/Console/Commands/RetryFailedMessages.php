d6fc20264af69dac70690e0da864846bc10c2897
yusufkarbackk <yusufkarback64@gmail.com>
2025-03-27 16:47:21 +0700

    process to create retry to send data feature

--- app/Console/Commands/RetryFailedMessages.php
+++ app/Console/Commands/RetryFailedMessages.php
@@ -6,6 +6,7 @@ use App\Models\DatabaseConfig;
 use App\Models\DatabaseTable;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Redis;
+use PDO;
 
 class RetryFailedMessages extends Command
 {
@@ -28,20 +29,69 @@ class RetryFailedMessages extends Command
      */
     public function handle()
     {
-        $databases = DatabaseConfig::all();
+        $redisProcessStream = new ProcessRedisStreams();
+        $tables = DatabaseTable::all();
 
-        foreach ($databases as $db) {
-            if ($this->isDatabaseOnline($db)) {
-                $retryKey = "retry:{$db->name}";
+        foreach ($tables as $table) {
+            $consumerGroup = $table->consumer_group;
+            $retryKey = "retry:{$consumerGroup}";
 
-                while (Redis::llen($retryKey) > 0) {
+            // Check if messages are waiting for this consumer group
+            while (Redis::llen($retryKey) > 0) {
+
+                if ($redisProcessStream->isDatabaseOnline($table->database->host)) {
+                    dump("Database {$table->database->name} is still down. Holding message...");
+                } else {
+                    dump("Database {$table->database->name} is up");
                     $message = json_decode(Redis::lpop($retryKey), true);
-                    $table = DatabaseTable::where('table_name', $message['table'])->first();
+                    $targetTable = DatabaseTable::where('table_name', $message['table'])->first();
 
-                    if ($table) {
-                        $this->insertData($table, $message['data']);
+                    try {
+                        $pdo = new PDO(
+                            "{$table->database->connection_type}:host={$table->database->host};dbname={$table->database->database_name}",
+                            $table->database->username,
+                            $table->database->password,
+                            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+                        );
+
+                        //var_dump($pdo);
+
+                        // Build insert query
+                        $columns = implode(', ', array_keys($message['table']));
+                        //var_dump($columns);
+                        //Log::info("column" . $columns);
+                        $values = implode(', ', array_fill(0, count($message['table']), '?'));
+                        //var_dump($values);
+                        //Log::info("column" . $values);
+
+
+                        $sql = "INSERT INTO {$table->table_name} ({$columns}) VALUES ({$values})";
+                        //var_dump($sql);
+                        $stmt = $pdo->prepare($sql);
+                        //var_dump($stmt);
+                        if ($stmt->execute(array_values($message['table']))) {
+                            //var_dump("Insert success");
+                        } else {
+                            //var_dump($stmt->errorInfo());
+                        }
+
+                        return $pdo->lastInsertId();
+                    } catch (\Exception $th) {
+                        dump($th->getMessage());
+                        //throw $th;
                     }
                 }
+
+                // $message = json_decode(Redis::lpop($retryKey), true);
+                // $targetTable = DatabaseTable::where('table_name', $message['table'])->first();
+
+                // if ($targetTable) {
+                //     if (!$redisProcessStream->isDatabaseOnline($table->database->host)) {
+                //         dump("Database {$table->database->name} is still down. Holding message...");
+                //     } else {
+                //         $redisProcessStream->insertData($table, $message['data']);
+                //     }
+                // }
             }
         }
     }
